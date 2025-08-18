d36736a30edc7c90e96c4c08b20d3df943cfac29
themindseyestudio <rick@themindseyestudio.com>
2025-08-17 20:55:57 -0500

    Update main.py

--- src/main.py
+++ src/main.py
@@ -71,5 +71,7 @@ def serve_assets(filename):
     return send_from_directory(os.path.join(app.static_folder, 'assets'), filename)
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5001, debug=True)
+    import os
+port = int(os.environ.get('PORT', 5001))
+app.run(host='0.0.0.0', port=port, debug=True)
 
